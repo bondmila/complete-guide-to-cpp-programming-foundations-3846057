92c363319f6030588cecdd05c45a488ec1838da5
bondmila <milab4981@gmail.com>
2025-06-11 16:50:22 +0000

    added mods

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main(){ 
+    std::string name;
+    std::cout << "Enter your name: " << std::flush;
+    std::cin >> name;
+    std::cout << "Nice to meet you, " << name << "!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+}
+    
